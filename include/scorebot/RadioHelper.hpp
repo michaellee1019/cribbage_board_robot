53c6f64d3e5fd8488a4ed17c3e0fbec3a7a7c423
Ryan Timmons <ryan.timmons@mongodb.com>
2024-02-12 13:43:27 -0500

    move to RadioHelper

---
+++ include/scorebot/RadioHelper.hpp
@@ -0,0 +1,30 @@
+#ifndef RADIOHELPER_HPP
+#define RADIOHELPER_HPP
+
+#include <RF24.h>
+
+// Send toSend and invoke callback if successfully sent.
+template <typename T, typename F>
+[[nodiscard]] bool doSend(RF24* radio, T* toSend, F&& callback) {
+    bool sent = radio->write(toSend, sizeof(T));
+    if (sent) {
+        callback();
+    }
+    return sent;
+}
+
+template <typename T>
+bool doRead(RF24* radio, T* out) {
+    if (radio->isAckPayloadAvailable()) {
+        radio->read(out, sizeof(T));
+        return true;
+    }
+    return false;
+}
+
+template <typename T>
+void doAck(RF24* radio, uint8_t pipe, T* acked) {
+    radio->writeAckPayload(pipe, acked, sizeof(T));
+}
+
+#endif
--- src/Devices.cpp
+++ src/Devices.cpp
@@ -3,6 +3,7 @@
 #include <string>
 
 #include <scorebot/Devices.hpp>
+#include <scorebot/RadioHelper.hpp>
 #include <scorebot/LeaderBoard.hpp>
 #include <scorebot/Message.hpp>
 #include <scorebot/PlayerBoard.hpp>
@@ -10,33 +11,6 @@
 // Cruft
 #define CE_PIN 10
 #define CSN_PIN 9
-
-
-namespace {
-// Send toSend and invoke callback if successfully sent.
-template <typename T, typename F>
-[[nodiscard]] bool doSend(RF24* radio, T* toSend, F&& callback) {
-    bool sent = radio->write(toSend, sizeof(T));
-    if (sent) {
-        callback();
-    }
-    return sent;
-}
-
-template <typename T>
-bool doRead(RF24* radio, T* out) {
-    if (radio->isAckPayloadAvailable()) {
-        radio->read(out, sizeof(T));
-        return true;
-    }
-    return false;
-}
-
-template <typename T>
-void doAck(RF24* radio, uint8_t pipe, T* acked) {
-    radio->writeAckPayload(pipe, acked, sizeof(T));
-}
-}  // namespace
 
 
 struct Button {
