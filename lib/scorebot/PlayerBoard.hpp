3789b41e4f7077c4267ceba83c784abb454c94f9
Ryan Timmons <ryan.timmons@mongodb.com>
2024-03-25 20:02:48 -0400

    hey it compiles

--- lib/scorebot/PlayerBoard.hpp
+++ lib/scorebot/PlayerBoard.hpp
@@ -5,6 +5,20 @@
 
 #include "TabletopBoard.hpp"
 
+const byte slaveAddresses[N_PLAYERS][5] = {
+    {'R', 'x', 'A', 'A', 'A'},
+    {'R', 'x', 'A', 'A', 'B'},
+    {'R', 'x', 'A', 'A', 'C'},
+};
+
+inline const byte* myBoardAddress() {
+    static_assert(BOARD_ID < N_PLAYERS);
+    if constexpr (BOARD_ID == -1 || BOARD_ID == 0) {
+        return slaveAddresses[0];
+    }
+    return slaveAddresses[BOARD_ID];
+}
+
 class PlayerBoard final : public TabletopBoard {
 public:
     explicit PlayerBoard(const IOConfig& config, TimestampT startupGeneration);
