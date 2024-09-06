922e101280f3d105447ed71d88a542a77dba63d0
Ryan Timmons <ryan.timmons@mongodb.com>
2024-09-06 11:23:21 -0400

    remove some cruft; refactor setup methods

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,11 @@
 #include "esp_chip_info.h"
 #include "Arduino.h"
 
-
-// https://randomnerdtutorials.com/esp-now-esp32-arduino-ide/
-
-/*
-  Rui Santos & Sara Santos - Random Nerd Tutorials
-  Complete project details at https://RandomNerdTutorials.com/get-change-esp32-esp8266-mac-address-arduino/
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
 #include <WiFi.h>
 #include <esp_wifi.h>
+
+
+// https://randomnerdtutorials.com/esp-now-esp32-arduino-ide/
 
 void readMacAddress(){
     uint8_t baseMac[6];
@@ -28,40 +22,6 @@ void readMacAddress(){
     } else {
         Serial.println("Failed to read MAC address");
     }
-}
-
-
-// https://tutoduino.fr/en/discover-freertos-on-an-esp32-with-platformio/
-const char *model_info(esp_chip_model_t model)
-{
-    switch (model)
-    {
-        case CHIP_ESP32:
-            return "ESP32";
-        case CHIP_ESP32S2:
-            return "ESP32S2";
-        case CHIP_ESP32S3:
-            return "ESP32S3";
-        case CHIP_ESP32C3:
-            return "ESP32C3";
-        case CHIP_ESP32H2:
-            return "ESP32H2";
-        default:
-            return "Unknown";
-    }
-}
-void print_chip_info()
-{
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("Chip model %s with %d CPU core(s), WiFi%s%s, ",
-           model_info(chip_info.model),
-           chip_info.cores,
-           (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-           (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
-    unsigned major_rev = chip_info.revision / 100;
-    unsigned minor_rev = chip_info.revision % 100;
-    printf("silicon revision v%d.%d\n", major_rev, minor_rev);
 }
 
 #define LED_PIN D0       // Define the pin for the LED
@@ -90,17 +50,21 @@ void LEDTask(void *pvParameters) {
     }
 }
 
-void setup() {
+void serialSetup() {
     Serial.begin(115200);
     Serial.println("Serial setup begin.");
     while (!Serial) {
         // wait for serial port to connect
     }
     Serial.println("Serial setup");
+}
 
+void wifiSetup() {
     WiFi.mode(WIFI_STA);
     WiFi.begin();
+}
 
+void buttonSetup() {
     // Initialize the LED and button pins
     pinMode(LED_PIN, OUTPUT);
     pinMode(BUTTON_PIN, INPUT);
@@ -115,6 +79,12 @@ void setup() {
     // Create the LED task
     xTaskCreate(LEDTask, "LEDTask", 1024, NULL, 1, NULL);
     Serial.println("xTaskCreate'd LEDTask");
+}
+
+void setup() {
+    serialSetup();
+    wifiSetup();
+    buttonSetup();
 }
 
 void loop() {
