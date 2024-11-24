c5081e94ecc0bfd24c00bd65708799ad32f24325
Ryan Timmons <ryan@rytim.com>
2024-11-24 15:52:39 -0500

    reformat

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,16 @@
+#include "utils.hpp"
 #include <Arduino.h>
+#include <HardwareSerial.h>
 #include <WiFi.h>
 #include <esp_now.h>
 #include <rom/rtc.h>
-#include <HardwareSerial.h>
-#include "utils.hpp"
 
 const int MODULE_SIZE = 2;
 RTC_DATA_ATTR static int rebootCount = -1;
 
 
 /* function headers */
-void appTask(void * parameter);
+void appTask(void* parameter);
 void initEspnow();
 
 
@@ -22,26 +22,27 @@ uint32_t msAfterESPNowRecv = 0;
 uint32_t counter = 0;
 
 void setup() {
-  /* Stack size in bytes. */ /* Parameter passed as input of the task */ /* Priority of the task. */
-  // xTaskCreate(appTask, "appTask", 10000, NULL, 2, NULL);
+    /* Stack size in bytes. */ /* Parameter passed as input of the task */ /* Priority of the task.
+                                                                            */
+    // xTaskCreate(appTask, "appTask", 10000, NULL, 2, NULL);
 
-  rebootCount++;
-  Serial.begin(115200);
+    rebootCount++;
+    Serial.begin(115200);
     while (!Serial) {
         // Serial.print("!Serial");
     }
     Serial.println("Serial.begin()!");
 
-  print_reset_reason(rtc_get_reset_reason(0));
-  verbose_print_reset_reason(rtc_get_reset_reason(0));
+    print_reset_reason(rtc_get_reset_reason(0));
+    verbose_print_reset_reason(rtc_get_reset_reason(0));
 
-  WiFi.disconnect();
-  initEspnow();
+    WiFi.disconnect();
+    initEspnow();
 }
 
 esp_now_peer_info_t slave;
 
-void send_cb(const uint8_t *mac_addr, esp_now_send_status_t status) {
+void send_cb(const uint8_t* mac_addr, esp_now_send_status_t status) {
     // sentCnt++;
     Serial.println("SENT!");
 };
@@ -51,8 +52,8 @@ void send_cb(const uint8_t *mac_addr, esp_now_send_status_t status) {
 // Setup and find own number.
 // Each number knows which goes before it.
 // Red: memory="after blue" (turn nr % players, who last went, sth)
-// Red: Broadcasts to everyone its current turn number and ~~which player nr just went~~ own player nr
-// Blue: Sees that Red broadcasted [turn_numberRed=0, red]
+// Red: Broadcasts to everyone its current turn number and ~~which player nr just went~~ own player
+// nr Blue: Sees that Red broadcasted [turn_numberRed=0, red]
 //
 
 // my player number = my mac address
@@ -70,7 +71,7 @@ I know it's my turn, and I keep track of the turn number
 Upon press I send everyone my state
 */
 
-void recv_cb(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
+void recv_cb(const uint8_t* mac_addr, const uint8_t* data, int data_len) {
     Serial.println("RECV!");
     memcpy(&slave.peer_addr, mac_addr, 6);
     esp_err_t addStatus = esp_now_add_peer(&slave);
@@ -79,8 +80,7 @@ void recv_cb(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
         printf("\n=====================");
         printf("\nADD PEER status=0x%02x", ESP_OK);
         printf("\n=====================\n");
-    }
-    else {
+    } else {
         printf("\n=====================");
         printf("\nADD PEER [FAILED] status=0x%02x", addStatus - ESP_ERR_ESPNOW_BASE);
         printf("\n=====================\n");
@@ -88,7 +88,7 @@ void recv_cb(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
 
     uint8_t time = 1;
     esp_err_t result = esp_now_send(mac_addr, &time, 1);
-    if (result == ESP_OK)   {
+    if (result == ESP_OK) {
         Serial.println("esp_now_send [ESP_OK]");
     }
     counter++;
@@ -96,35 +96,33 @@ void recv_cb(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
 }
 
 void registerCallbacks() {
-  esp_now_register_send_cb(send_cb);
-  esp_now_register_recv_cb(recv_cb);
+    esp_now_register_send_cb(send_cb);
+    esp_now_register_recv_cb(recv_cb);
 }
 void initEspnow() {
-  bzero(&slave, sizeof(slave));
-  WiFi.mode(WIFI_AP_STA);
-  delay(10);
-  strcpy(sta_mac, WiFi.macAddress().c_str());
-  strcpy(softap_mac, WiFi.softAPmacAddress().c_str());
-  Serial.printf("STA MAC: %s\r\n", sta_mac);
-  Serial.printf(" AP MAC: %s\r\n", softap_mac);
-  if (esp_now_init() == ESP_OK) {
-    Serial.printf("ESPNow Init Success\n");
-    registerCallbacks();
-  }
-  else {
-    Serial.printf("ESPNow Init Failed\n");
-    ESP.restart();
-  }
+    bzero(&slave, sizeof(slave));
+    WiFi.mode(WIFI_AP_STA);
+    delay(10);
+    strcpy(sta_mac, WiFi.macAddress().c_str());
+    strcpy(softap_mac, WiFi.softAPmacAddress().c_str());
+    Serial.printf("STA MAC: %s\r\n", sta_mac);
+    Serial.printf(" AP MAC: %s\r\n", softap_mac);
+    if (esp_now_init() == ESP_OK) {
+        Serial.printf("ESPNow Init Success\n");
+        registerCallbacks();
+    } else {
+        Serial.printf("ESPNow Init Failed\n");
+        ESP.restart();
+    }
 }
 
-void appTask(void * parameter)
-{
+void appTask(void* parameter) {
     // rtc->setup();
     // pinMode(EXT_WDT_PIN, OUTPUT);
     while (1) {
         yield();
-      // rtc->loop();
-      // lcd->loop();
+        // rtc->loop();
+        // lcd->loop();
     }
 }
 
@@ -145,7 +143,8 @@ void appTask(void * parameter)
 //   Serial.begin(115200);
 //
 //   while (!Serial) {
-//     ; // wait for serial port to connect. Needed for native USB, on LEONARDO, MICRO, YUN, and other 32u4 based boards.
+//     ; // wait for serial port to connect. Needed for native USB, on LEONARDO, MICRO, YUN, and
+//     other 32u4 based boards.
 //   }
 //   Serial.println("setup");
 //
@@ -155,9 +154,8 @@ void appTask(void * parameter)
 //     ,  "Blink"   // A name just for humans
 //     ,  2048  // This stack size can be checked & adjusted by reading the Stack Highwater
 //     ,  NULL
-//     ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-//     ,  NULL
-//     , 1); // You don't even have a core
+//     ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the
+//     lowest. ,  NULL , 1); // You don't even have a core
 //
 //   xTaskCreatePinnedToCore(
 //     TaskAnalogRead
@@ -170,7 +168,8 @@ void appTask(void * parameter)
 //
 //     Serial.println("tasks started");
 //
-//   // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
+//   // Now the task scheduler, which takes over control of scheduling individual tasks, is
+//   automatically started.
 // }
 //
 // void loop(){};
