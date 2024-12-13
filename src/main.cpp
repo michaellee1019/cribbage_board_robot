d7b18b803ef3def0aa50d535a8713128f66448c4
Ryan Timmons <ryan.timmons@mongodb.com>
2024-12-13 13:43:24 -0500

    idk

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 
+#define LED_BUILTIN 2
+
 // define two tasks for Blink & AnalogRead
 void TaskBlink( void *pvParameters );
 void TaskAnalogRead( void *pvParameters );
@@ -7,13 +9,13 @@ void TaskAnalogRead( void *pvParameters );
 // the setup function runs once when you press reset or power the board
 void setup() {
 
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+    // initialize serial communication at 9600 bits per second:
+    Serial.begin(115200);
 
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB, on LEONARDO, MICRO, YUN, and other 32u4 based boards.
-  }
-  Serial.println("setup");
+    while (!Serial) {
+        ; // wait for serial port to connect. Needed for native USB, on LEONARDO, MICRO, YUN, and other 32u4 based boards.
+    }
+    Serial.println("setup");
 
   // Now set up two tasks to run independently.
   xTaskCreatePinnedToCore(
@@ -39,54 +41,26 @@ void setup() {
   // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
 }
 
-void loop()
-{
-      Serial.println("looped");
+void loop() { }
 
-  // Empty. Things are done in Tasks.
-}
-
-/*--------------------------------------------------*/
-/*---------------------- Tasks ---------------------*/
-/*--------------------------------------------------*/
-
-void TaskBlink(void *pvParameters)  // This is a task.
-{
+void TaskBlink(void *pvParameters)  {
   (void) pvParameters;
-
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, LEONARDO, MEGA, and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN takes care
-  of use the correct LED pin whatever is the board used.
-
-  The MICRO does not have a LED_BUILTIN available. For the MICRO board please substitute
-  the LED_BUILTIN definition with either LED_BUILTIN_RX or LED_BUILTIN_TX.
-  e.g. pinMode(LED_BUILTIN_RX, OUTPUT); etc.
-
-  If you want to know what pin the on-board LED is connected to on your Arduino model, check
-  the Technical Specs of your board  at https://www.arduino.cc/en/Main/Products
-
-  This example code is in the public domain.
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-*/
 
   // initialize digital LED_BUILTIN on pin 13 as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.println("Before for!");
 
+  unsigned long its = 0;
   for (;;) // A Task shall never return or exit.
   {
     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
     vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
     vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
+    Serial.println("Blink!");
+      if (its % 1000 == 0) {
+          Serial.println("Blink!");
+      }
   }
 }
 
@@ -108,7 +82,7 @@ void TaskAnalogRead(void *pvParameters)  // This is a task.
     // read the input on analog pin 0:
     int sensorValue = analogRead(A0);
     // print out the value you read:
-    Serial.println(sensorValue);
+    // Serial.println(sensorValue);
     vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
   }
 }
